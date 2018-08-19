0ac35f4d6cf5259d27cb8497a9cedffc3812fdbb
chandu044 <41730209+chandu044@users.noreply.github.com>
2018-08-19 13:41:04 +0530

    Create p1.java

---
+++ p1.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,i;
+		Scanner s=new Scanner(System.in);
+		String s1=s.next();
+		a=nextInt();
+		char[] a=s1.toCharArray();
+		for(i=0;i<n;i++)
+		{
+			System.out.print(a[i]);
+		}
+		
+	}
+}
