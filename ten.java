1aedb203f59bc15ee963ecf4632687cce392c3c3
chandu044 <41730209+chandu044@users.noreply.github.com>
2018-08-21 11:13:49 +0530

    Create ten.java

---
+++ ten.java
@@ -0,0 +1,20 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int a,r,n;
+	Scanner s=new Scanner(System.in);
+	n=s.nextInt();
+	r=n/10;
+	a=(r+1)*10;
+	System.out.print(a);
+	}
+	
+}
